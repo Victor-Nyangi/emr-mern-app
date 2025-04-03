25711de2e1f96748373c5cf043fc5d3e45576cd7
Victor <gichuivictor@gmail.com>
2025-04-03 17:08:11 +0300

    feat(settings): Setup account and profile pages

---
+++ app/(admin)/settings/layout.tsx
@@ -0,0 +1,55 @@
+import { Separator } from "@/components/ui/separator";
+import { SidebarNav } from "@/components/settings/SidebarNav";
+
+const sidebarSettingsItems = [
+  {
+    title: "Profile",
+    href: "/settings",
+  },
+  {
+    title: "Account",
+    href: "/settings/account",
+  },
+  {
+    title: "Appearance",
+    href: "/settings/appearance",
+  },
+  {
+    title: "Notifications",
+    href: "/settings/notifications",
+  },
+  {
+    title: "Display",
+    href: "/settings/display",
+  },
+];
+
+interface SettingsLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function SettingsLayout({
+  children,
+}: SettingsLayoutProps) {
+  return (
+    <>
+      <main>
+        <div className="space-y-6 p-8 pb-16 md:block">
+          <div className="space-y-0.5">
+            <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+            <p className="text-muted-foreground">
+              Manage your account settings and set e-mail preferences.
+            </p>
+          </div>
+          <Separator className="my-6" />
+          <div className="flex space-y-8 flex-row space-x-12">
+            <aside>
+              <SidebarNav items={sidebarSettingsItems} />
+            </aside>
+            <div className="flex-1">{children}</div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
